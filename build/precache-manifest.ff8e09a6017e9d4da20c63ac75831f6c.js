4546d487bcfe16bc0c126363cb784652af3d512c
Prithwish Dey <49427529+PRITHWISHDEY@users.noreply.github.com>
2020-09-06 00:24:54 +0530

    Add files via upload

---
+++ build/precache-manifest.ff8e09a6017e9d4da20c63ac75831f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eea4ecd3cbb9d832c07e50f93b5d712",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63459191e072285e25f",
+    "url": "/static/css/main.50806e6a.chunk.css"
+  },
+  {
+    "revision": "690c5e57b68b4cc31121",
+    "url": "/static/js/2.562a9181.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.562a9181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63459191e072285e25f",
+    "url": "/static/js/main.a45804b4.chunk.js"
+  },
+  {
+    "revision": "20efb6170f09280cdcf8",
+    "url": "/static/js/runtime-main.a74124dd.js"
+  }
+]);
